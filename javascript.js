6eae1c98f24c7c88a92701ea2e693b37507d9ed1
Matheus de Souza Berto da Silveira <131717441+matheussouzabsilveira@users.noreply.github.com>
2025-04-22 18:44:46 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,42 @@
+
+
+function menu(){
+    let aside = document.querySelector("#aside")
+
+    let currentLeft = window.getComputedStyle(aside).left;
+
+    if (currentLeft === "-300px") {
+        aside.style.left = "0px";
+    } else if (currentLeft === "0px") {
+        aside.style.left = "-300px";
+    }
+
+}
+
+function asideOff(){
+    let larguraTela = window.innerWidth
+    let aside = document.querySelector("#aside")
+    if(larguraTela > 720){
+        aside.style.left = "-300px"
+    }
+}
+
+function card5(){
+    window.location.href = "tecnico.html"
+}
+
+function card4(){
+    window.location.href = "humanas.html"
+}
+
+function card3(){
+    window.location.href = "mtm.html"
+}
+
+function card2(){
+    window.location.href = "linguagens.html"
+}
+
+function card1(){
+    window.location.href = "ciencias.html"
+}
